76a17e4d492000774ae213a54d0bd46b83f9d1d1
CrYptOz007 <gerrick1cheok@hotmail.com>
2024-01-25 16:28:30 +0800

    feat: added ipmi info route

---
+++ backend/internal/controllers/services/ipmi/ipmi.go
@@ -0,0 +1,44 @@
+package ipmi
+
+import (
+	"strconv"
+
+	"github.com/CrYptOz007/Fusion/internal/helpers"
+	encryption "github.com/CrYptOz007/Fusion/internal/helpers"
+	"github.com/CrYptOz007/Fusion/internal/models/service"
+	"github.com/CrYptOz007/Fusion/internal/server/utils"
+	"github.com/CrYptOz007/Fusion/pkg/ipmitool"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func GetInfo(c echo.Context) error {
+	database := utils.GetLocal[*gorm.DB](c, "dbClient")
+
+	id := c.QueryParam("id")
+
+	parsedId, err := strconv.Atoi(id)
+	if err != nil {
+		return helpers.ReturnUnexpectedError(c, []string{err.Error()})
+	}
+
+	// Get service from database
+	service, err := service.FetchServiceWithUser(parsedId, database)
+	if err != nil {
+		return helpers.ReturnUnexpectedError(c, []string{err.Error()})
+	}
+
+	password, err := encryption.Decrypt(service.Password, service.User.Password, service.User.Salt)
+	if err != nil {
+		return helpers.ReturnUnexpectedError(c, []string{err.Error()})
+	}
+
+	client, err := ipmitool.NewClient(service.Hostname, uint16(service.Port), service.Username, password)
+	if err != nil {
+		return helpers.ReturnUnexpectedError(c, []string{err.Error()})
+	}
+
+	info := client.GetInfo()
+
+	return c.JSON(200, info)
+}
--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/CrYptOz007/Fusion/internal/controllers/auth"
 	"github.com/CrYptOz007/Fusion/internal/controllers/services"
+	"github.com/CrYptOz007/Fusion/internal/controllers/services/ipmi"
 	"github.com/CrYptOz007/Fusion/internal/controllers/services/proxmox"
 	"github.com/CrYptOz007/Fusion/internal/controllers/user"
 	"github.com/CrYptOz007/Fusion/internal/middlewares"
@@ -21,6 +22,8 @@ func Register(app *echo.Echo, groups types.Groups) {
 		r.GET("/proxmox/nodes", proxmox.GetNodes)
 		r.GET("/proxmox/virtualmachines", proxmox.GetVirtualMachines)
 		r.GET("/proxmox/containers", proxmox.GetContainers)
+
+		r.GET("/ipmi/info", ipmi.GetInfo)
 	})
 
 	r.Group("auth", nil, func(r *Router) {
