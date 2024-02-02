236cc0d77d784810b536161bbed91c61ec2c99e9
CrYptOz007 <gerrick1cheok@hotmail.com>
2024-02-02 09:27:28 +0800

    deps: add flowbite, sveltequery and axios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	mode: process.env.ENVIRONMENT || 'development',
+	server: {
+		port: 4000
+	},
 	plugins: [sveltekit()]
 });
