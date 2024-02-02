ffead70dd54b32cc2546f3fea3ca2fe533bdb8d6
CrYptOz007 <gerrick1cheok@hotmail.com>
2024-02-02 09:28:58 +0800

    chore: add global queryclient for svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.pcss';
+	import { Navbar, NavBrand } from 'flowbite-svelte';
+	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
+
+	const queryClient = new QueryClient();
 </script>
 
-<slot />
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">Fusion</span>
+	</NavBrand>
+</Navbar>
+
+<QueryClientProvider client={queryClient}>
+	<slot />
+</QueryClientProvider>
